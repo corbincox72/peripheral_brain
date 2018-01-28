0c066e3e794468ec0f47e2d0b4d815c0dacd355a
Corbin Cox <corbincox72@gmail.com>
2018-01-27 21:11:42 -0500

    Dosing key and Pk Trough AG Dosing

--- javascript/aminoglycosidePkTrough.js
+++ javascript/aminoglycosidePkTrough.js
@@ -1,16 +1,17 @@
 function k() {
-  return (0.0024 * crcl() + 0.01).toFixed(4);
+  return (Math.log(ptPk()/ptTrough())/(ptTau()-ptInfTime())).toFixed(4);
 }
 
+function volume() {
+  return (ptDose() * (1 - Math.exp(-1*k()*ptInfTime())) / (ptInfTime() * k() * (ptCmax() - (ptCmin() * Math.exp(-1*k()*ptInfTime())) ))).toFixed(1);
+}
 
-function crcl() {
-  if (document.getElementById("crclCKDEpi").checked) {
-    return ckdEpiBSA();
-  } else if (document.getElementById("crclCG").checked){
-    return cgCrCl();
-  } else {
-    return document.getElementById("customCrClEntry").value;
-  }
+function ptCmax() {
+  return (ptPk() / Math.exp(-1*k()*ptTimeToSample()));
+}
+
+function ptCmin() {
+  return (ptTrough() * Math.exp(-1*k()*ptTroughMinInterval()));
 }
 
 function volumeWt() {
@@ -21,61 +22,4 @@ function volumeWt() {
   } else {
     return ibw();
   }
-}
-
-function lowVolume() {
-  return ((document.getElementById("lowVdkg").value) * volumeWt()).toFixed(1);
-}
-
-function highVolume() {
-  return ((document.getElementById("highVdkg").value) * volumeWt()).toFixed(1);
-}
-
-function volume() {
-  return document.getElementById("vd").value;
-}
-
-function tau() {
-  return document.getElementById("tau").value;
-}
-
-function tauCalc() {
-  var res = parseFloat(Math.log(cmaxD()/cminD()) / k()) + parseFloat(tinf());
-  return res.toFixed(2);
-}
-
-function cpeakD() {
-  return document.getElementById("cpeakD").value;
-}
-
-function cminD() {
-  return document.getElementById("cminD").value;
-}
-
-function tinf() {
-  return document.getElementById("tinf").value;
-}
-
-function timeToSample() {
-  return document.getElementById("timeToSample").value;
-}
-
-function maintenanceDoseCalc() {
-  return (cmaxD() * tinf() * volume() * k() * (1-Math.exp(-1*k()*tau())) / (1-Math.exp(-1*k()*tinf()))).toFixed(0);
-}
-
-function md() {
-  return document.getElementById("md").value;
-}
-
-function cmaxE() {
-  return (md() * (1-Math.exp(-1*k()*tinf())) / volume() / tinf() / k() / (1-Math.exp(-1*k()*tau()))).toFixed(1);;
-}
-
-function cpkE() {
-  return (cmaxE() * Math.exp(-1*k()*timeToSample())).toFixed(1);;
-}
-
-function cminE() {
-  return (cmaxE() * Math.exp(-1*k()*(tau() - tinf()))).toFixed(1);
 }
--- javascript/commonFunctions.js
+++ javascript/commonFunctions.js
@@ -3,43 +3,16 @@ function printResult(id,func) {
   document.getElementById(id).innerHTML = func;
 }
 
-/* Common Variables */
-
-function wt() {
-  return document.getElementById("wt").value;
+/* Basic Pharmacokinetics */
+function halfLife() {
+  return (Math.log(2)/k()).toFixed(2);
 }
 
-function ht() {
-  return document.getElementById("ht").value;
+function cmaxD() {
+  return cpeakD() / Math.exp(-1*k()*timeToSample())
 }
 
-function age() {
-  return document.getElementById("age").value;
-}
-
-function scr() {
-  return document.getElementById("scr").value;
-}
-
-function sex() {
-  if (document.getElementById("sexF").checked === true) {
-    return "F";
-  } else {
-    return "M";
-  }
-}
-
-function race() {
-  if (document.getElementById("raceAA").checked == true) {
-    return "AA";
-  } else {
-    return "W";
-  }
-}
-
-
-/* Body Weights */
-
+/* Weights */
 function ibw() {
   if (sex() == "F") {
     return Math.max(0,(45.5 + 2.3 * (ht()-60)).toFixed(1));
@@ -86,49 +59,4 @@ function renalWeightUsed() {
 
 function bsa() {
   return Math.sqrt((ht() * wt() * 2.205) / 3131).toFixed(2);
-}
-
-/* Renal Function */
-
-function cgCrCl() {
-  if (sex() == "F") {
-    return ((140-age())*renalWeight()/72/scr()*0.85).toFixed(0);
-  } else {
-    return ((140-age())*renalWeight()/72/scr()).toFixed(0);
-  }
-}
-
-function ckdEpi() {
-  if (sex() == "M") {
-    var k = 0.9;
-    var a = -0.411;
-    var sexMultiplier = 1;
-  } else {
-    var k = 0.7;
-    var a = -0.329;
-    var sexMultiplier = 1.018;
-  }
-
-  if (race() == "AA") {
-    var raceMultiplier = 1.159;
-  } else {
-    var raceMultiplier = 1;
-  }
-
-  return (141 * Math.pow(Math.min(1,scr()/k),a) * Math.pow(Math.max(1,scr()/k),-1.209) * Math.pow(0.993,age()) * sexMultiplier * raceMultiplier).toFixed(0) ;
-
-}
-
-function ckdEpiBSA() {
-  return (ckdEpi() * bsa() / 1.73).toFixed(0);
-}
-
-
-/* Basic Pharmacokinetics */
-function halfLife() {
-  return (Math.log(2)/k()).toFixed(2);
-}
-
-function cmaxD() {
-  return cpeakD() / Math.exp(-1*k()*timeToSample())
 }
