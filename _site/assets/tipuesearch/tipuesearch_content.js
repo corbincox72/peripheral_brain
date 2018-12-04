31142e4b59caed7aa4c16bcc139e1a291dfc495e
Corbin Cox <corbincox72@gmail.com>
2018-12-04 11:04:49 -0500

    Altered mobile footer

--- _site/assets/tipuesearch/tipuesearch_content.js
+++ _site/assets/tipuesearch/tipuesearch_content.js
@@ -95,7 +95,7 @@ var tipuesearch = {"pages": [{
     "url": "/peripheral_brain/drugClasses/clinicalPk.html"
   },{
     "title": "Vasopressors",
-    "text": "Push-Dose EPI 1 Mixing 9mL NS in 10mL syringe Add 1 mL (100mcg) cardiac epinephrine (0.1mg/mL) Caution: above is for 1mg/10mL (0.1mg/mL) premix syringes, EPI vials are 1mg/1mL Resulting solution is 100mcg/10mL (10mcg/mL) Epinephrine Admin 0.5-2mL (5-20 mcg) Q2-5min PRN of above solution\\ Doses last $\\approx$ 10min\\ If mixing not possible, give 0.5mL dose of pure cardiac epi (50mcg)\\ DO NOT ADMINISTER 1mg EPI TO INDIVIDUALS NOT IN CARDIAC ARREST References Weingart S. Push-dose pressors for immediate blood pressure control. Clinical and Experimental Emergency Medicine. 2015;2(2):131-132. doi:10.15441/ceem.15.010 &#8617;",
+    "text": "Push-Dose EPI 1 Mixing 9mL NS in 10mL syringe Add 1 mL (100mcg) cardiac epinephrine (0.1mg/mL) Caution: above is for 1mg/10mL (0.1mg/mL) premix syringes, EPI vials are 1mg/1mL Resulting solution is 100mcg/10mL (10mcg/mL) Epinephrine Admin 0.5-2mL (5-20 mcg) Q2-5min PRN of above solution Doses last ≈ 10min If mixing not possible, give 0.5mL dose of pure cardiac epi (50mcg) DO NOT ADMINISTER 1mg EPI TO INDIVIDUALS NOT IN CARDIAC ARREST References Weingart S. Push-dose pressors for immediate blood pressure control. Clinical and Experimental Emergency Medicine. 2015;2(2):131-132. doi:10.15441/ceem.15.010 &#8617;",
     "tags": "",
     "url": "/peripheral_brain/drugClasses/pressors.html"
   }]};
